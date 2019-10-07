a96cc16577254a7df72bd5f8d54654a7f820a3e4
Youssef El Saadany <youssef.saadany@gmail.com>
2019-10-07 22:58:21 +0200

    deleted old file

--- test.py
+++
@@ -1,9 +0,0 @@
-import generator
-
-res = generator.extract_docstring('<python_file.py>')
-md = generator.generate_markdown(res)
-
-with open('test.md', 'w') as doc:
-    for line in md:
-        doc.write(line)
-        doc.write("\n")
