e991af665ec66d0398294ba0357e7e825e58af29
Youssef El Saadany <youssef.saadany@gmail.com>
2019-11-03 12:00:27 +0100

    new setup.py file

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+setup(
+        name=SimpleDocGenerator,
+        version=0.1.0,
+        packages_dir={"package": "simpledoc"},
+        author="Youssef El Saadany",
+        author_email="youssef.saadany@gmail.com",
+        description="This package can be used to easily generate documention in markdown for Python projects",
+        url="https://github.com/yElSaadany/simple_doc_generator/",
+        keywords="documentation markdown python project simple",
+        project_urls={
+            "Source Code": "https://github.com/yElSaadany/simple_doc_generator"
+            },
+        install_requires=[
+            'docstring-parser==0.3',
+            'setuptools==41.0.1',
+            ],
+        extras_requires={
+            'dev': [
+                "flake8"
+                ]
+            },
+        entry_points={
+            "console_scripts": [
+                'generate-doc = simpledoc.main:main'
+                ]
+            }
+)
