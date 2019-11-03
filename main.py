7410026e27bb08cf403882ac91614ccadcb76182
Youssef El Saadany <youssef.saadany@gmail.com>
2019-11-03 12:01:05 +0100

    some files moving

--- main.py
+++ main.py
@@ -3,16 +3,12 @@ from utils.arguments import init_args, check_files
 from extractor import extract_docstring_from_indexes
 
 
-def main(args):
+def main():
+    args = init_args()
+    check_files(args.i, args.o)
     input_file = args.i
     output_file = args.o
     name = input("Name of your project: ")
     docstrings = extract_docstring_from_indexes(input_file)
     res = gen.generate_markdown(docstrings, name)
     gen.create_doc_file(res, output_file)
-
-
-if __name__ == '__main__':
-    args = init_args()
-    check_files(args.i, args.o)
-    main(args)
---
+++ tests/test_extractor.py
@@ -0,0 +1,3 @@
+import extractor as ex
+
+print(is_signature)
