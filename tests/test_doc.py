5538b3784656e0ef05e036abbfba4cd75680748d
Youssef El Saadany <youssef.saadany@gmail.com>
2019-11-03 10:06:03 +0100

    new tests folder

---
+++ tests/test_doc.py
@@ -0,0 +1,36 @@
+def function1(arg1):
+    """This is the first test function
+
+    The point is to test my documentation generator.
+
+    Args:
+        arg1 (int): First parameters
+
+    Returns:
+        Nothing.
+    """
+    pass
+
+
+def squared(x):
+    """Returns x squared
+
+    Args:
+        x (number): The number you want squared
+
+    Returns:
+        x squared.
+    """
+    return x**2
+
+
+def hello(name):
+    """Greets you
+
+    Args:
+        name (str): Your name
+
+    Returns:
+        Returns a string telling you hello.
+    """
+    return "hello " + name
