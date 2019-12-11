fb26bc3dee31f6552565237b919a1aa7cc999bb5
Youssef El Saadany <youssef.saadany@gmail.com>
2019-12-11 22:04:32 +0100

    you can specify a module within a directory

--- simpledoc/main.py
+++ simpledoc/main.py
@@ -1,14 +1,20 @@
 import simpledoc.generator as gen
 from simpledoc.utils.arguments import init_args, check_files
-from simpledoc.extractor import extract_docstring_from_indexes
+from simpledoc.extractor import extract_docstrings
+import os
+import sys
 
 
 def main():
     args = init_args()
     check_files(args.i, args.o)
-    input_file = args.i
+    if len(args.i.split('/')) > 1:
+        current_dir = os.getcwd()
+        os.chdir('/'.join(args.i.split('/')[:-1]))
+    module = args.i.split('/')[-1][:-3]
     output_file = args.o
-    name = input("Name of your project: ")
-    docstrings = extract_docstring_from_indexes(input_file)
-    res = gen.generate_markdown(docstrings, name)
+    sys.path.append('')
+    docstrings = extract_docstrings(module)
+    os.chdir(current_dir)
+    res = gen.generate_markdown2(docstrings)
     gen.create_doc_file(res, output_file)
